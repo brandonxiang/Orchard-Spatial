6f79684b0979e192ecc1ecce010daf1b5ccf40c1
brandonxiang <1542453460@qq.com>
2016-12-22 14:22:16 +0800

    init My.Spatial

---
+++ src/Orchard.Web/Modules/My.Spatial/Migrations.cs
@@ -0,0 +1,28 @@
+﻿using Orchard.Data.Migration;
+
+namespace My.Spatial
+{
+    public class Migrations : DataMigrationImpl {
+        public int Create() {
+            SchemaBuilder.CreateTable("TestRecord", table => table
+                .Column<int>("Id", column => column.PrimaryKey().Identity())
+
+                );
+            return 1;
+        }
+
+        public int UpdateFrom1() {
+            SchemaBuilder.AlterTable("TestRecord", table => table
+                .AddColumn<GeoAPI.Geometries.IGeometry>("Geometry"));
+            return 2;
+        }
+//
+//        public int UpdateFrom2() {
+//            SchemaBuilder.AlterTable("TestRecordddd", table => table
+//                .AddColumn<string>("Code"));
+//            SchemaBuilder.AlterTable("TestRecordddd", table => table
+//                .AddColumn<GeoAPI.Geometries.IGeometry>("Geometry"));
+//            return 3;
+//        }
+    }
+}
